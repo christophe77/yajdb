87e685a1cf2e122b4d15e046a961cd029316f394
christophe bellec <christophe.bellec@ekim.fr>
2022-05-10 15:04:28 +0200

    dist 1

--- dist/cjs/operations/database/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { create, createAsync } from "./create";
-import { drop, dropAsync } from "./drop";
-declare const database: {
-    create: typeof create;
-    drop: typeof drop;
-    createAsync: typeof createAsync;
-    dropAsync: typeof dropAsync;
-};
-export default database;
--- dist/cjs/operations/table/update.d.ts
+++
@@ -1,3 +0,0 @@
-import { CommonOperationResponse, StringMap } from "../../types/operations";
-export declare function update(dbName: string, tableName: string, searchPayload: StringMap, replacePayload: StringMap): CommonOperationResponse;
-export declare function updateAsync(dbName: string, tableName: string, searchPayload: StringMap, replacePayload: StringMap): Promise<CommonOperationResponse>;
